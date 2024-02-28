f69df7dd045a09490e900f3f09bfc1d910efa585
iantanui <rutooian@gmail.com>
2024-02-28 16:42:19 +0300

    display in  card

--- app/src/main/java/com/example/myweather/MainActivity.kt
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myweather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -25,10 +27,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myweather.ui.theme.MyweatherTheme
@@ -108,23 +110,39 @@ fun WeatherApp() {
             )
             Spacer(modifier = Modifier.height(16.dp))
         }
+        if (apiResponse.temp.isNotEmpty() && apiResponse.humidity.isNotEmpty()) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+            ) {
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(
+                        text = "Temperature: ${apiResponse.temp}",
+                        modifier = Modifier.fillMaxWidth()
+                    )
 
-        Text(
-            text = "Temperature: ${apiResponse.temp}",
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Text(
-            text = "Humidity: ${apiResponse.humidity}",
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
+                    Text(
+                        text = "Humidity: ${apiResponse.humidity}",
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+        }
     }
 
     LaunchedEffect(isSearchClicked) {
         if (isSearchClicked && searchText.isNotBlank()) {
-            apiResponse = getWeatherFromApi(searchText)
+            val response = getWeatherFromApi(searchText)
+            if (response.temp.isNotEmpty() && response.humidity.isNotEmpty()) {
+                apiResponse = response
+                errorMessage = ""
+            } else {
+                // Display error
+                errorMessage = "Weather details not found"
+            }
             isSearchClicked = false
         }
     }
